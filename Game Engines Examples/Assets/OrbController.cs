e4e2498b938edc2d939d4f3265e502c4b74e1636
PhilToolan <c17433026@mydit.ie>
2020-10-23 16:08:20 +0100

    Up to lab 4

---
+++ Game Engines Examples/Assets/OrbController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbController : MonoBehaviour
+{
+
+    public TankController tankController;
+    public FpsController fpsController;
+    public EnemyTankController enemyTankController;
+    public RotateMe rotateMe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter()
+    {
+        if (col.gameObject.tag == "Main Camera")
+        {
+            !tankController.enabled = tankController.enabled;
+            fpsController.enabled = !fpsController.enabled;
+            enemyTankController.enabled = !enemyTankController.enabled;
+            rotateMe.enabled = !rotateMe.enabled;
+        }
+    }
+
+    void OnTriggerStay()
+    {
+
+    }
+}
